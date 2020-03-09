2ce272242201f05019dd0c714ebbe89b1fe34d29
LwandoG <lwandogqobho@gmail.com>
2020-03-09 15:56:40 +0200

    The ultimate commit

---
+++ calculation.py
@@ -0,0 +1,8 @@
+import math
+#getting the values from the user
+a =int(input("Enter a"))
+b =int(input("Enter b"))
+c =int(input("Enter c"))
+d =int(input("Enter d"))
+ans = math.sqrt(a-3)/(b**2 + c**2 + d**2)
+print(ans)
